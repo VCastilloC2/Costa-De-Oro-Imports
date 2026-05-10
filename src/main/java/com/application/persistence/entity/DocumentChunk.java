55cd39ebdd91a073829d3e8b6faa56216f8c4093
Víctor José Castillo Castro <castillocastrovictorjose@hotmail.com>
2026-05-09 22:40:12 -0500

    Update All

--- src/main/java/com/application/persistence/entity/DocumentChunk.java
+++ src/main/java/com/application/persistence/entity/DocumentChunk.java
@@ -2,13 +2,10 @@ package com.application.persistence.entity;
 
 import lombok.*;
 
-@Data
 @Getter
 @Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
-@NoArgsConstructor
-@Builder
 public class DocumentChunk {
     private String id;
     private String source;
