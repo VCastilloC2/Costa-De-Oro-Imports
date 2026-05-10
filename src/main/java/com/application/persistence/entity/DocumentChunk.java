e2a2c05cf530cd03cd074f59351e04b71f66fc57
YUBLIAN VERBEL <yublianverbel@gmail.com>
2026-05-09 22:50:42 -0500

    Sopas

--- src/main/java/com/application/persistence/entity/DocumentChunk.java
+++ src/main/java/com/application/persistence/entity/DocumentChunk.java
@@ -2,12 +2,11 @@ package com.application.persistence.entity;
 
 import lombok.*;
 
-@Data
+
 @Getter
 @Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
-@NoArgsConstructor
 @Builder
 public class DocumentChunk {
     private String id;
